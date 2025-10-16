6833c3c28d60d5eb8ae58787d7d9109881ca42f6
Isidore-M <isidore.mbargamanga@triosstudent.com>
2025-10-15 23:39:27 -0400

    Initial Commit

---
+++ Assignment5/Assignment5App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment5App.swift
+//  Assignment5
+//
+//  Created by Eezy Mongo on 2025-10-15.
+//
+
+import SwiftUI
+
+@main
+struct Assignment5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment5Tests/Assignment5Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Assignment5Tests.swift
+//  Assignment5Tests
+//
+//  Created by Eezy Mongo on 2025-10-15.
+//
+
+import Testing
+
+struct Assignment5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
